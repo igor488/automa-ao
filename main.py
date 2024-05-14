7debd69da9f7957f569653e7d54edb9e03d9fa8c
igor488 <igorgabrieldasilva98@gmail.com>
2024-05-14 02:04:04 -0300

    pyautogui

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui
+
+import time
+
+pyautogui.PAUSE = 4
+
+pyautogui.press("win")
+
+
+pyautogui.write("edge")
+
+pyautogui.press("enter")
+
+pyautogui.write("youtube.com")
+
+pyautogui.press("enter")
